b9d3af393cddeb98d7611c851a7c436d3e081a44
Gabriel Silva <gabriel.silva@redfox.tech>
2022-10-24 15:09:33 -0300

    added: auto generate prisma schema file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '@prisma/prisma.module';
+import { PrismaModule } from '@prisma/module/prisma.module';
 import { UserModule } from './packages';
 import { GraphqlModule } from './infra/graphql';
 
--- src/packages/user/modules/user/user.service.ts
+++ src/packages/user/modules/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@prisma/prisma.service';
+import { PrismaService } from '@prisma/module/prisma.service';
 import { CreateUserDto, UpdateUserDto } from '@dtos';
 import {
   AlreadyExistsException,

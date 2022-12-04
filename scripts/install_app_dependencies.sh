e4b9e914dfa074e7b8f783e46d40651ca3242b47
Gabriel Silva <gabriel.silva@redfox.tech>
2022-12-03 23:53:08 -0300

    update ci

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+/bin/bash ./load_nvm.sh
+
 cd /home/ec2-user/app/
 npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+/bin/bash ./load_nvm.sh
+
 cd /home/ec2-user/app/
 npm run build
 npm start:dev

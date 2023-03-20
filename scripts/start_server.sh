a8ec0bb4f96f2dfed7941452ec5154dbec756b3c
Gabriel Silva <gabrieljsilva2301@gmail.com>
2023-03-20 17:16:49 -0300

    remove: ecs files

--- scripts/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/ec2-user/.bash_profile
-source /home/ec2-user/.bashrc
-
-sudo chmod -R 777 /home/ec2-user/app/
-cd /home/ec2-user/app/
-npm run build
-npm start:dev
-sudo chmod -R 775 /home/ec2-user/app/
